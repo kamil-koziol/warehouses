f980469da85135e7d583c9848842b048f429085d
Wleszcz <wiktor.leszczynski@tlen.pl>
2023-11-06 20:12:21 +0100

    format

--- db.py
+++ db.py
@@ -1,9 +1,6 @@
-import sqlalchemy
 import sqlalchemy.orm
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Boolean, DateTime, ForeignKey
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
 
 engine = sqlalchemy.create_engine("sqlite:///db.sqlite", echo=False)
 Base = sqlalchemy.orm.declarative_base()
@@ -13,9 +10,10 @@ Session.configure(bind=engine)  # once engine is available
 
 session = Session()
 
+
 class Instruktor(Base):
     __tablename__ = 'instruktor'
-    
+
     id = Column(Integer, primary_key=True)
     imie = Column(String(20))
     nazwisko = Column(String(30))
@@ -25,9 +23,10 @@ class Instruktor(Base):
     data_zatrudnienia = Column(Date)
     plec = Column(Boolean)
 
+
 class Krusant(Base):
     __tablename__ = 'krusant'
-    
+
     id = Column(Integer, primary_key=True)
     imie = Column(String(20))
     nazwisko = Column(String(30))
@@ -35,18 +34,20 @@ class Krusant(Base):
     data_urodzenia = Column(Date)
     plec = Column(Boolean)
 
+
 class Kurs(Base):
     __tablename__ = 'kurs'
-    
+
     id = Column(Integer, primary_key=True)
     ko_kursant_id = Column(Integer, ForeignKey('krusant.id'))
     ko_instruktor_id = Column(Integer, ForeignKey('instruktor.id'))
     data_rozpoczecia = Column(Date)
     data_zakonczenia = Column(Date, nullable=True)
 
+
 class Zajecia(Base):
     __tablename__ = 'zajecia'
-    
+
     id = Column(Integer, primary_key=True)
     ko_kurs_id = Column(Integer, ForeignKey('kurs.id'))
     ko_samochod_id = Column(Integer, ForeignKey('samochod.id'))
@@ -54,19 +55,22 @@ class Zajecia(Base):
     koniec = Column(DateTime)
     sie_odbyly = Column(Boolean)
 
+
 class Samochod(Base):
     __tablename__ = 'samochod'
-    
+
     id = Column(Integer, primary_key=True)
     marka = Column(String(20))
     rok_produkcji = Column(Integer)
     model = Column(String(30))
 
+
 class DaneUdostepnionePrzezWORD(Base):
     __tablename__ = 'dane_udostepnione_przez_word'
-    
+
     id = Column(Integer, primary_key=True)
     zdano = Column(Boolean)
     data_czas_egzaminu = Column(DateTime)
+
 
 Base.metadata.create_all(engine)
