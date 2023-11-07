8414f329063993caf8c725688c4814b0c8d5acdc
Wleszcz <wiktor.leszczynski@tlen.pl>
2023-11-07 17:15:43 +0100

    poprawki 4

--- db/scripts.py
+++ db/scripts.py
@@ -4,9 +4,9 @@ from db import *
 
 
 def get_number_of_active_courses(session: Session):
-    return session.query(Kurs).filter(Kurs.data_zakonczenia.isnot(None)).count()
+    return session.query(Kurs).filter(Kurs.data_zakonczenia is not None).count()
 
 
 
 def get_number_of_active_instructors(session: Session):
-    return session.query(Instruktor).filter(Instruktor.data_zwolnienia.isnot(None)).count()
+    return session.query(Instruktor).filter(Instruktor.data_zwolnienia is not None).count()
